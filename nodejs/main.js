d23a0a88c1a35e89342038af2949670b030c0290
AnHyoungBin <nks0005@naver.com>
2020-07-19 19:27:07 +0900

    동적으로 파일이 올라오면 이를 적용
    동적으로 파일이 올라오면 이를 적용

--- nodejs/main.js
+++ nodejs/main.js
@@ -14,32 +14,25 @@ var app = http.createServer(function(request, response) {
   if (pathname === '/') {
     if (queryData.id === undefined) {
 
-      var title = 'Welcome';
-      var description = 'Hello, Node.js';
-      var template = `
-      <!doctype html>
-      <html>
-      <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1><a href="/">WEB</a></h1>
-        <ul>
+
+      fs.readdir('./data', function(err, filelist) {
+        var title = 'Welcome';
+        var description = 'Hello, Node.js';
+
+        console.log(filelist);
+        /*var list =
+        `<ul>
           <li><a href="/?id=HTML">HTML</a></li>
           <li><a href="/?id=CSS">CSS</a></li>
           <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ul>
-        <h2>${title}</h2>
-        <p>${description}</p>
-      </body>
-      </html>
-      `;
-      response.writeHead(200);
-      response.end(template);
+        </ul>`;*/
 
-    } else {
-      fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
+        var list = '<ul>';
+        for (var i = 0; i < filelist.length; i++) {
+          list += '<li><a href="/?id=' + filelist[i] + '">' + filelist[i] + '</a></li>';
+        }
+        list += '</ul>';
+
 
         var template = `
       <!doctype html>
@@ -50,11 +43,7 @@ var app = http.createServer(function(request, response) {
       </head>
       <body>
         <h1><a href="/">WEB</a></h1>
-        <ul>
-          <li><a href="/?id=HTML">HTML</a></li>
-          <li><a href="/?id=CSS">CSS</a></li>
-          <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ul>
+        ${list}
         <h2>${title}</h2>
         <p>${description}</p>
       </body>
@@ -62,6 +51,47 @@ var app = http.createServer(function(request, response) {
       `;
         response.writeHead(200);
         response.end(template);
+      });
+
+
+    } else {
+      fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
+        fs.readdir('./data', function(err, filelist) {
+
+          /*var list =
+          `<ul>
+            <li><a href="/?id=HTML">HTML</a></li>
+            <li><a href="/?id=CSS">CSS</a></li>
+            <li><a href="/?id=JavaScript">JavaScript</a></li>
+          </ul>`;*/
+
+          console.log(filelist);
+          var list = '<ul>';
+          for (var i = 0; i < filelist.length; i++) {
+            list += '<li><a href="/?id=' + filelist[i] + '">' + filelist[i] + '</a></li>';
+          }
+          list += '</ul>';
+
+
+
+          var template = `
+      <!doctype html>
+      <html>
+      <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1><a href="/">WEB</a></h1>
+        ${list}
+        <h2>${title}</h2>
+        <p>${description}</p>
+      </body>
+      </html>
+      `;
+          response.writeHead(200);
+          response.end(template);
+        });
       });
     }
   } else {
