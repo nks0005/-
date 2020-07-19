f060bdd006f515f2512218c0fb1dc02a7bc5931e
AnHyoungBin <nks0005@naver.com>
2020-07-19 18:57:30 +0900

    404페이지, 조건 페이지 구현
    404페이지, 조건 페이지 구현

--- nodejs/main.js
+++ nodejs/main.js
@@ -2,33 +2,20 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
-var args = process.argv;
-console.log(args);
-console.log(args[2]);
-process.argv.forEach(function(val, index, array){
-  console.log(index + ': ' + val);
-  console.log(array);
-});
+
+var app = http.createServer(function(request, response) {
+  var _url = request.url;
+  var queryData = url.parse(_url, true).query;
+  var pathname = url.parse(_url, true).pathname;
+  var title = queryData.id;
 
 
-var app = http.createServer(function(request,response){
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    var title = queryData.id;
 
-    if(_url == '/'){
-      title = 'Welcome';
-    }
-    if(_url == '/favicon.ico'){
-      return response.writeHead(404);
-    }
-    console.log(queryData.id);
-    response.writeHead(200);
-    fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
-      if(err)
-      {
-        description = "";
-      }
+  if (pathname === '/') {
+    if (queryData.id === undefined) {
+
+      var title = 'Welcome';
+      var description = 'Hello, Node.js';
       var template = `
       <!doctype html>
       <html>
@@ -48,8 +35,39 @@ var app = http.createServer(function(request,response){
       </body>
       </html>
       `;
+      response.writeHead(200);
       response.end(template);
-    })
+
+    } else {
+      fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
+
+        var template = `
+      <!doctype html>
+      <html>
+      <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1><a href="/">WEB</a></h1>
+        <ul>
+          <li><a href="/?id=HTML">HTML</a></li>
+          <li><a href="/?id=CSS">CSS</a></li>
+          <li><a href="/?id=JavaScript">JavaScript</a></li>
+        </ul>
+        <h2>${title}</h2>
+        <p>${description}</p>
+      </body>
+      </html>
+      `;
+        response.writeHead(200);
+        response.end(template);
+      });
+    }
+  } else {
+    response.writeHead(404);
+    response.end('Not found');
+  }
 
 
 });
