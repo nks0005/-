a79a5466ed6e85ecd90b52b175519fe84e771d4b
AnHyoungBin <nks0005@naver.com>
2020-08-06 04:34:32 +0900

    db
    db

--- NodeJS_MySQL/main.js
+++ NodeJS_MySQL/main.js
@@ -4,17 +4,9 @@ var url = require('url');
 var qs = require('querystring');
 var template = require('./lib/template.js');
 var path = require('path');
-var sanitizeHtml = require('sanitize-html');
 
-var mysql = require('mysql');
-var db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '111111',
-  database: 'opentutorials'
-});
+var db = require('./lib/db');
 
-db.connect();
 
 
 
