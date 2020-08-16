31866e3b54352111cf001ded182234895118d7f5
AnHyoungBin <nks0005@naver.com>
2020-08-16 13:57:04 +0900

    Cookie 개념
    Cookie 개념 및 설정

--- NodeJS_Cookie/cookie.js
+++ NodeJS_Cookie/cookie.js
@@ -1,6 +1,27 @@
 var http = require('http');
+var cookie = require('cookie');
 
-http.createServer(function(request, response){
-    // response.writeHead(200, { 'Set-Cookie': ['yummy_cookie=choco', 'tasty_cookie=strawberry'] });
-    response.end('Cookie!!');
+http.createServer(function (request, response) {
+
+    // npm install cookie를 통해 모듈을 사용하자.
+    console.log(request.headers.cookie);
+    var cookies = {};
+    if (request.headers.cookie !== undefined) {
+
+        cookies = cookie.parse(request.headers.cookie);
+        console.log(cookies.yummy_cookie);
+
+
+    }
+    response.writeHead(200, { 'Set-Cookie':
+    [
+        'yummy_cookie=choco',
+        'tasty_cookie=strawberry',
+        `Permanent=cookies; Max-age=${60*60*24*30}`,
+        `secure=secure; Secure`,
+        `httponly=httponly; HttpOnly`,
+        `path=path; path=/cookie`,
+        `Domain=Domain; Domain=o2.org`
+    ]});
+    response.end(`Cookie!!`);
 }).listen(3000);
